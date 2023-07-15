1b84a9786b2cfd4b66c3dd2c3c88fac9a7b74442
Eu Geng <84378694+shadeonline@users.noreply.github.com>
2023-07-16 00:33:06 +0800

    Updated Stamp modal

--- components/StampCard.js
+++ components/StampCard.js
@@ -1,6 +1,14 @@
-import { View, StyleSheet, Image, Text } from 'react-native';
-
+import React, { useState } from 'react';
+import { StyleSheet, View, Image, Text, TouchableOpacity, Modal } from 'react-native';
+import StampTaskModal from './StampTaskModal';
 const StampCard = ({ rows, stampsPerRow, progress }) => {
+  const [selectedStamp, setSelectedStamp] = useState(null);
+
+  const handleStampPress = (stampKey) => {
+    setSelectedStamp(stampKey);
+    console.log(stampKey)
+  };
+
   const renderStampRows = () => {
     const stamps = [];
 
@@ -14,23 +22,34 @@ const StampCard = ({ rows, stampsPerRow, progress }) => {
         if (isLastStamp) {
           // Add treasure chest image to the last stamp in each row
           row.push(
-            <View key={stampKey} style={styles.stamp}>
-              <Image source={require('../assets/treasure-chest.png')} style={styles.crownImage} />
+            <TouchableOpacity
+              key={stampKey}
+              id='reward'
+              style={styles.stamp}
+              onPress={() => handleStampPress(stampKey)}
+            >
+              <Image
+                source={require('../assets/treasure-chest.png')}
+                style={styles.crownImage}
+              />
               {isFilled && (
                 <Image source={require('../assets/stamp.png')} style={styles.stampImage} />
               )}
-
-            </View>
+            </TouchableOpacity>
           );
         } else {
           row.push(
             // Add numbers to each stamp and render img on top if stamp is filled
-            <View key={stampKey} style={styles.stamp}>
+            <TouchableOpacity
+              key={stampKey}
+              style={styles.stamp}
+              onPress={() => handleStampPress(stampKey)}
+            >
               <Text>{stampKey + 1}</Text>
               {isFilled && (
                 <Image source={require('../assets/stamp.png')} style={styles.stampImage} />
               )}
-            </View>
+            </TouchableOpacity>
           );
         }
       }
@@ -40,14 +59,22 @@ const StampCard = ({ rows, stampsPerRow, progress }) => {
         </View>
       );
     }
-
     return stamps;
+  };
+
+  const handleCloseModal = () => {
+    setSelectedStamp(null);
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Stamps Collected!</Text>
       {renderStampRows()}
+      <StampTaskModal
+        selectedStamp={selectedStamp}
+        progress={progress}
+        onCloseModal={handleCloseModal} 
+        stampsPerRow = {stampsPerRow}/>
     </View>
   );
 };
@@ -57,14 +84,12 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 25,
     paddingVertical: 10,
-    
   },
   container: {
     width: '100%',
     paddingHorizontal: 10,
     paddingVertical: 20,
     justifyContent: 'center',
-
   },
   stampRow: {
     flexDirection: 'row',
---
+++ components/StampTaskModal.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Image, Text, TouchableOpacity, Modal } from 'react-native';
+
+const StampTaskModal = ({ selectedStamp, progress, onCloseModal, stampsPerRow }) => {
+  const renderModalContent = () => {
+    if (((selectedStamp + 1) % stampsPerRow === 0 && progress < selectedStamp)) {
+      return (
+        <Text style={styles.modalText}>Reward: 1 free drink</Text>
+      );
+    }
+
+    if (progress <= selectedStamp) {
+      return (
+        <Text style={styles.modalText}>
+          Visit the restaurant and eat a meal to fill up this stamp!
+        </Text>
+      );
+    }
+    if (((selectedStamp + 1) % stampsPerRow === 0 && progress > selectedStamp)) {
+      return (
+        <>
+          <Text style={styles.modalText}>Completed!</Text>
+          <Text style={styles.modalText}>Please claim your reward in the rewards tab.</Text>
+        </>
+      );
+    }
+    if (progress > selectedStamp) {
+      return (
+        <Text style={styles.modalText}>Completed!</Text>
+      );
+    }
+  }
+
+
+  return (
+    <Modal visible={selectedStamp !== null} animationType="fade" transparent>
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          {renderModalContent()}
+          <TouchableOpacity style={styles.closeButton} onPress={onCloseModal}>
+            <Text style={styles.closeButtonText}>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+  },
+  modalText: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  closeButton: {
+    backgroundColor: 'lightgrey',
+    padding: 10,
+    borderRadius: 8,
+  },
+  closeButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default StampTaskModal
