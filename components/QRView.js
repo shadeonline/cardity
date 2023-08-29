2b9dfe6c101d1283f4e6a0ea809d252e5be9e45a
Eu Geng <84378694+shadeonline@users.noreply.github.com>
2023-08-29 20:13:53 +0800

    Generate QR Code Component

--- components/QRView.js
+++ components/QRView.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Text, View, Image, TouchableOpacity } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
+
 
 const QRView = ({ card }) => {
-
-    console.log(card);
+    const cardDetails = 'cardity:' + JSON.stringify(card);
+  
     return (
-        <View>
-        </View>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>QR Code for {card.cardName}</Text>
+        <Text>{cardDetails}</Text>
+        
+        <QRCode
+          value={cardDetails}
+          size={200}
+          color="black"
+          backgroundColor="white"
+        />
+      </View>
     );
-};
-
-export default QRView;
+  };
+  
+  export default QRView;
+  
